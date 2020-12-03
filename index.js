aa5ba500ed94dd954a315603d723b199a8c82868
lorenrd <lrondan95@gmail.com>
2020-12-03 21:58:35 +0000

    Added support to nedb for GET/POST methods

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+var DataStore = require('nedb')
 
 var port = 3000;
 var BASE_API_PATH="/api/v1";
-
-var contacts = [
-    {name: "Peter", age: 12},
-    {name: "John", age: 33}
-];
+var DB_FILE_NAME = __dirname + "/products.json"
 
 console.log("Starting API server...");
 
 var app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res ) => {
-    res.send("<html><body><h1>HOLA MUNDO</h1></body></html>")
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
 });
 
-app.get(BASE_API_PATH + "/contacts", async (req, res) => {
-    console.log(Date() + "-GET /contacts");
-    res.send(contacts);
-})
+app.get("/", (req, res ) => {
+    res.send("<html><body><h1>Coffaine Products</h1></body></html>")
+});
 
-app.post(BASE_API_PATH + "/contacts", (req, res) => {
-    console.log(Date() + "-POST /contacts");
-    var contact = req.body;
-    contacts.push(contact);
-    res.sendStatus(201);
-})
+app.get(BASE_API_PATH + "/products", async (req, res) => {
+    console.log(Date() + "-GET /products")
+    db.find({}).exec(function (err, products) {
+        res.send(products)
+    });
+});
+
+app.post(BASE_API_PATH + "/products", (req, res) => {
+    console.log(Date() + "-POST /products")
+    var product = req.body
+    db.insert(product, (err) => {
+        if(err){
+            console.error(Date() + " - " + err)
+            res.send(500)
+        }else{
+            res.sendStatus(201)
+        }
+    });
+});
 
 app.listen(port);
 

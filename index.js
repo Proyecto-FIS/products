ab6d5756d34c7783f304e138fcee0b0e0e422eee
josperdom1 <josperdom1@gmail.com>
2020-12-08 08:52:38 +0100

    fix: Commiting changes to update from main

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const DataStore = require("nedb");
-//Swager documentation
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,25 +10,32 @@ const port = process.env.PORT || 3000;
 const BASE_API_PATH = "/api/v1";
 const DB_FILE_NAME = __dirname + "/products.json";
 
-// Extended: https://swagger.io/specification/#infoObject
-const swaggerOptions = {
+const expressSwagger = require("express-swagger-generator")(app);
+
+let options = {
   swaggerDefinition: {
     info: {
+      description: "Coffaine Products resources",
+      title: "Coffaine Products",
       version: "1.0.0",
-      title: "Coffaine products API",
-      description: "All products resources of coffaine documented in swagger",
-      contact: {
-        name: "Coffaine",
+    },
+    host: "localhost:3000",
+    basePath: BASE_API_PATH,
+    produces: ["application/json", "application/xml"],
+    schemes: ["http", "https"],
+    securityDefinitions: {
+      JWT: {
+        type: "apiKey",
+        in: "header",
+        name: "Authorization",
+        description: "",
       },
-      servers: ["http://localhost:3000" + BASE_API_PATH],
     },
   },
-  //Ruta donde haya endpoints ['.routes/*.js']
-  apis: ["index.js"],
+  //basedir: __dirname, //app absolute path
+  files: ["index.js"], //Path to the API handle folder
 };
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+expressSwagger(options);
 
 console.log("Starting API server...");
 
@@ -46,16 +50,6 @@ app.get("/", (req, res) => {
   );
 });
 
-//Routes
-/**
- * @swagger
- * /api/v1/products:
- *  get:
- *    description: Use to request all products
- *    responses:
- *      '200':
- *        description: A successful response
- */
 app.get(BASE_API_PATH + "/products", async (req, res) => {
   console.log(Date() + "-GET /products");
   db.find({}).exec(function (err, products) {
